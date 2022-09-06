f97d294202bc4058a3959501effd6befd82a0603
Rohinimirashe <rohinimirashe100@gmail.com>
2022-09-06 11:03:42 +0530

    Initialization

---
+++ README.md
@@ -0,0 +1,26 @@
+-------------------
+# Cocktails Website
+
+Cocktails website to display list of all the cocktails along with their details. Developed using cocktails DB api
+
+
+---------------------
+## Hosted Site here
+
+https://cocktails-11.netlify.app/
+
+-------------------
+## Installation
+
+Install my-project with npm
+
+```bash
+  npm install my-project
+  cd my-project
+```
+    
+## Screenshots
+
+![App Screenshot](https://i.imgur.com/j3hm5B2.png)
+![App Screenshot](https://i.imgur.com/QiOzt9T.png)
+
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      The company itself is a very successful company. The trouble of
+      repudiating the architect that you receive in the offices, I will open to
+      follow and endure them, but the elders, unless often someone here
+      pursues us with hatred of the truth as if I were to explain the blinded
+      and the pain? Is it pleasing to reason that the error of flattery is here
+      spurred on?
+      </p>
+    </section>
+  );
+}
